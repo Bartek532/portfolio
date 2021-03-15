2cc0012ba10f965ba8ee12befcf0376ee39640f7
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-03-15 16:50:31 +0100

    add logo and fix navbar

---
+++ js/menu.js
@@ -0,0 +1,29 @@
+AOS.init();
+
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.5,
+};
+
+const observer = new IntersectionObserver(entries => {
+  if (entries[0].isIntersecting) {
+    underlineMenuItemById(entries[0].target.id);
+  }
+}, options);
+
+document.querySelectorAll("section").forEach(section => {
+  observer.observe(section);
+});
+
+const underlineMenuItemById = id => {
+  const links = document.querySelectorAll(".menu__links__link");
+
+  links.forEach(link => {
+    link.classList.remove("menu__links__link--active");
+  });
+
+  document
+    .querySelector(`a[href="#${id}"]`)
+    .classList.add("menu__links__link--active");
+};
---
+++ js/mobile_menu.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector(".nav__hamburger");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("nav__hamburger--active");
+  menu.classList.toggle("menu--active");
+  document.body.classList.toggle("--stop-scrolling");
+});
+
+const linksList = document.querySelector(".menu__links");
+
+linksList.addEventListener("click", e => {
+  if (e.target.classList.contains("menu__links__link")) {
+    hamburger.classList.toggle("nav__hamburger--active");
+    menu.classList.toggle("menu--active");
+    document.body.classList.remove("--stop-scrolling");
+  }
+});
